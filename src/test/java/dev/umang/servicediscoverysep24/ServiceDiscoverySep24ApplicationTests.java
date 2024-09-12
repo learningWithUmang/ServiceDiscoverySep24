b404ffc06f4b8aed47d58bdc29edc8d03cbe88b8
Umang Agrawal <skylags@Umangs-MacBook-Pro.local>
2024-09-12 23:14:13 +0530

    service discovery

---
+++ src/test/java/dev/umang/servicediscoverysep24/ServiceDiscoverySep24ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.umang.servicediscoverysep24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoverySep24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
